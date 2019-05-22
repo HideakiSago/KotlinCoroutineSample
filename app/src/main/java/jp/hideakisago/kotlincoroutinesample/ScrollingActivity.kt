afc23805d8215190937846860c7a45983cc2295c
hideaki.sago <hideaki.sago@gmail.com>
2019-05-22 09:00:48 +0900

    format all

--- app/src/main/java/jp/hideakisago/kotlincoroutinesample/ScrollingActivity.kt
+++ app/src/main/java/jp/hideakisago/kotlincoroutinesample/ScrollingActivity.kt
@@ -1,10 +1,10 @@
 package jp.hideakisago.kotlincoroutinesample
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_scrolling.*
 
 class ScrollingActivity : AppCompatActivity() {
@@ -15,7 +15,7 @@ class ScrollingActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
--- app/src/test/java/jp/hideakisago/kotlincoroutinesample/ExampleUnitTest.kt
+++ app/src/test/java/jp/hideakisago/kotlincoroutinesample/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package jp.hideakisago.kotlincoroutinesample
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
