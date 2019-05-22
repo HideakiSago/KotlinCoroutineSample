18198368af9bcfc8486e35521e76fd6ffccb85ac
hideaki.sago <hideaki.sago@gmail.com>
2019-05-22 09:05:20 +0900

    launch の動作確認を追加

--- app/src/main/java/jp/hideakisago/kotlincoroutinesample/ScrollingActivity.kt
+++ app/src/main/java/jp/hideakisago/kotlincoroutinesample/ScrollingActivity.kt
@@ -1,11 +1,15 @@
 package jp.hideakisago.kotlincoroutinesample
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_scrolling.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ScrollingActivity : AppCompatActivity() {
 
@@ -17,6 +21,8 @@ class ScrollingActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        launchTest()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,5 +40,57 @@ class ScrollingActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    /**
+     * [launch] の動作確認です。
+     * また、 [delay] と [Thread.sleep] の違いも検証しています。
+     *
+     * 以下のような結果になります。
+     * ```
+     * 2019-05-22 09:07:50.823 14121-14121 D/ScrollingActivity: before launch main
+     * 2019-05-22 09:07:50.890 14121-14121 D/ScrollingActivity: after launch main
+     * 2019-05-22 09:07:50.917 14121-14180 D/ScrollingActivity: start launch1 DefaultDispatcher-worker-1
+     * 2019-05-22 09:07:50.917 14121-14181 D/ScrollingActivity: start launch2 DefaultDispatcher-worker-2
+     * 2019-05-22 09:07:51.020 14121-14181 D/ScrollingActivity: start launch3 DefaultDispatcher-worker-2
+     * 2019-05-22 09:07:56.919 14121-14180 D/ScrollingActivity: end   launch1 DefaultDispatcher-worker-1
+     * 2019-05-22 09:07:56.921 14121-14180 D/ScrollingActivity: start launch4 DefaultDispatcher-worker-1
+     * 2019-05-22 09:07:56.985 14121-14180 D/ScrollingActivity: end   launch2 DefaultDispatcher-worker-1
+     * 2019-05-22 09:07:57.021 14121-14181 D/ScrollingActivity: end   launch3 DefaultDispatcher-worker-2
+     * 2019-05-22 09:08:02.925 14121-14181 D/ScrollingActivity: end   launch4 DefaultDispatcher-worker-2
+     * ```
+     */
+    private fun launchTest() {
+        val sleepTime: Long = 6 * 1000
+        Log.d("ScrollingActivity", "before launch ${Thread.currentThread().name}")
+
+        GlobalScope.launch {
+            Log.d("ScrollingActivity", "start launch1 ${Thread.currentThread().name}")
+            // Thread.sleep だと thread 自体を停止してしまうが、
+            Thread.sleep(sleepTime)
+            Log.d("ScrollingActivity", "end   launch1 ${Thread.currentThread().name}")
+        }
+
+        GlobalScope.launch {
+            Log.d("ScrollingActivity", "start launch2 ${Thread.currentThread().name}")
+            // delay なら Thread を block するのではなく coroutine を中断するだけなので、
+            // 他の coroutine で Thread が使い回される。
+            delay(sleepTime)
+            Log.d("ScrollingActivity", "end   launch2 ${Thread.currentThread().name}")
+        }
+
+        GlobalScope.launch {
+            Log.d("ScrollingActivity", "start launch3 ${Thread.currentThread().name}")
+            Thread.sleep(sleepTime)
+            Log.d("ScrollingActivity", "end   launch3 ${Thread.currentThread().name}")
+        }
+
+        GlobalScope.launch {
+            Log.d("ScrollingActivity", "start launch4 ${Thread.currentThread().name}")
+            delay(sleepTime)
+            Log.d("ScrollingActivity", "end   launch4 ${Thread.currentThread().name}")
+        }
+
+        Log.d("ScrollingActivity", "after launch ${Thread.currentThread().name}")
     }
 }
